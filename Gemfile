6401fb17da14ce548424257a6981ef2d750c82b7
Anthony King <anthony@madetech.com>
2018-10-23 16:34:43 +0100

    set up semantic-logger for sinatra
    This adds support for tagged logging, as well as structured extra params.
    
    Note: this should be broken out into a separate package and done properly

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ ruby File.read('.ruby-version').chomp
 
 gem 'mysql2'
 gem 'puma'
+gem 'semantic_logger'
 gem 'sentry-raven'
 gem 'sequel', '~> 5.9'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,10 +1,63 @@
 require 'sequel'
 require 'sinatra/base'
 require 'sinatra/json'
+require 'semantic_logger'
+
+class SemanticLoggerMiddleware
+  def initialize(app, logger: nil)
+    @app = app
+    @logger = logger
+  end
+
+  def call(env)
+    env['rack.logger'] = @logger
+    env['Request-ID'] ||= SecureRandom.hex(8)
+
+    @logger.tagged(request_id: env['Request-ID']) do
+      log_start(env)
+      @app.call(env).tap do |status, headers, _body|
+        log_end(env, status, headers)
+      end
+    end
+  end
+
+  private
+
+  def log_start(env)
+    @logger.info(
+      'Request started',
+      address: env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-",
+      user: env["REMOTE_USER"] || "-",
+      time: Time.now.strftime("%d/%b/%Y:%H:%M:%S %z"),
+      method: env['REQUEST_METHOD'],
+      path: env['PATH_INFO'],
+      query: env['QUERY_STRING'],
+      http_version: env['HTTP_VERSION'],
+    )
+  end
+
+  def log_end(env, status, headers)
+    @logger.info(
+      'Request finished',
+      status: status.to_s[0..3],
+      length: headers['Content-Length'] || '-',
+    )
+  end
+end
+
 
 class App < Sinatra::Base
-  configure :production, :staging, :development do
-    enable :logging
+  set :logger, SemanticLogger[App]
+
+  configure do
+    SemanticLogger.default_level = :debug
+    SemanticLogger.add_appender(io: STDOUT, formatter: :color)
+
+    use SemanticLoggerMiddleware, logger: logger
+  end
+
+  configure :production do
+    SemanticLogger.default_level = :info
   end
 
   DB = Sequel.connect(
