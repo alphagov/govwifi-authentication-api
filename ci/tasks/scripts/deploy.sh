283a08d87d75d99144c737556b40310ad41198bd
Anthony King <anthony@madetech.com>
2019-04-29 14:44:59 +0100

    specify cluster and service in respective names

--- ci/tasks/scripts/deploy.sh
+++ ci/tasks/scripts/deploy.sh
@@ -8,7 +8,7 @@ function deploy() {
   local cluster_name service_name deploy_stage
 
   deploy_stage="$(stage_name)"
-  cluster_name="${deploy_stage}-${CLUSTER_NAME}-cluster"
+  cluster_name="${deploy_stage}-${CLUSTER_NAME}"
   service_name="${SERVICE_NAME}-${deploy_stage}"
 
   ecs_deploy_region \
