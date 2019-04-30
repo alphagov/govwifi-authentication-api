fdaf8bb8782d621716571dd1fa82cb070ce6deb6
Anthony King <anthony@madetech.com>
2019-04-30 15:07:07 +0100

    add in deployment tasks for the deployment pipeline (#78)
    * add in deployment tasks for the deployment pipeline
    
    * deploy to both regions
    
    * specify cluster and service in respective names
    
    * Update build-deployable.yml

---
+++ ci/tasks/scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -v -e -u -o pipefail
+
+source deploy-tools/aws-helpers.sh
+
+function deploy() {
+  local cluster_name service_name deploy_stage
+
+  deploy_stage="$(stage_name)"
+  cluster_name="${deploy_stage}-${CLUSTER_NAME}"
+  service_name="${SERVICE_NAME}-${deploy_stage}"
+
+  ecs_deploy_region \
+    "${cluster_name}" \
+    "${service_name}" \
+    "eu-west-1"
+
+  ecs_deploy_region \
+    "${cluster_name}" \
+    "${service_name}" \
+    "eu-west-2"
+}
+
+deploy
